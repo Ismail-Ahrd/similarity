5af43989180ceb73911b7f002d90ea72bc415cd2
Ismail-Ahrd <ahroudismail9@gmail.com>
2023-07-01 00:35:08 +0200

    updated

--- src/helpers/documentation-code.ts
+++ src/helpers/documentation-code.ts
@@ -1,7 +1,7 @@
 export const nodejs = `const axios = require("axios");
 const options = {
     method: 'POST',
-    url: 'https://similarityapi.com/api/v1/similarity',
+    url: 'https://similarity-kappa.vercel.app/api/v1/similarity',
     data: {
       text1: 'First text',
       text2: 'Second text'
@@ -18,7 +18,7 @@ axios.request(options).then(function (response) {
 });`
 
 export const python = `import requests
-url = 'https://similarityapi.com/api/v1/similarity'
+url = 'https://similarity-kappa.vercel.app/api/v1/similarity'
 api_key = 'YOUR_API_KEY'
 text1 = 'First text'
 text2 = 'Second text'
@@ -34,4 +34,4 @@ if response.status_code == 200:
     data = response.json()
     print(data)
 else:
-    print(f'Request failed with status code {response.status_code}')`
+    print('Request failed with status code {response.status_code}')`
