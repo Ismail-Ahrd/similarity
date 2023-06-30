2d403bb93ee045386c98c94d603de0c5f065ec3c
Ismail-Ahrd <ahroudismail9@gmail.com>
2023-06-30 21:33:27 +0200

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="en" className={cn('bg-white text-slate-900 antialiased', inter.className)}>
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
         <Providers>
+          {/* @ts-expect-error Server Component */}
           <Navbar />
           <main>{children}</main>
           <Toaster position='bottom-right'/>
